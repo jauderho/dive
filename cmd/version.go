140d8963288bbf284be68d062dbe2416a9fd7e0e
Adam Baxter <git@voltagex.org>
2024-09-30 20:27:14 +1000

    Revert version code in this branch
    Signed-off-by: Adam Baxter <git@voltagex.org>

--- cmd/version.go
+++ cmd/version.go
@@ -2,10 +2,17 @@ package cmd
 
 import (
 	"fmt"
-	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
+
+type Version struct {
+	Version   string
+	Commit    string
+	BuildTime string
+}
+
+var version *Version
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -18,12 +25,10 @@ func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
-func printVersion(cmd *cobra.Command, args []string) {
-	var bi, ok = debug.ReadBuildInfo()
-	if ok {
-		fmt.Printf("dive %s from %s\n", bi.Main.Version, bi.Main.Path)
-	} else {
-		fmt.Printf("dive (unknown version)")
-	}
+func SetVersion(v *Version) {
+	version = v
+}
 
+func printVersion(cmd *cobra.Command, args []string) {
+	fmt.Printf("dive %s\n", version.Version)
 }
